d615acfe81a84b99cea324d9045863986fb42aab
Thomas Rasmussen <tsn1983@gmail.com>
2019-02-09 16:05:37 +0100

    test

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-module.exports = function removeSpace(string) {
-    if (typeof string !== "string") throw new TypeError("Not a string!");
-    return string.replace(/\s/g, "");
-  };
+// module.exports = function removeSpace(string) {
+//     if (typeof string !== "string") throw new TypeError("Not a string!");
+//     return string.replace(/\s/g, "");
+//   };
+
+export class dtHelpers {
+  test() {
+    console.log('hello world');
+  }
+}
+
+export class regexHelpers{
+  regexTest(){
+    console.log("regex test");
+  }
+}
