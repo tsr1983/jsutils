ab95b46dbecf0d8e579f4b79e469a566894c2b66
Thomas Rasmussen <tsn1983@gmail.com>
2019-02-09 21:52:14 +0100

    fixes

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-module.exports = function removeSpace(string) {
+// module.exports = function removeSpace(string) {
+//     if (typeof string !== "string") throw new TypeError("Not a string!");
+//     return string.replace(/\s/g, "");
+//   };
+
+//   export default class DateUtils {
+//        DateTest(params) {
+
+//         console.log("DateTest");
+//     }
+//   }
+
+
+class Utils {
+  removeSpace(string) {
     if (typeof string !== "string") throw new TypeError("Not a string!");
     return string.replace(/\s/g, "");
   };
 
-  export default class DateUtils {
-       DateTest(params) {
-        
-        console.log("DateTest");
-    }
+
+  test2() {
+    console.log("test2");
   }
+}
+
+module.exports = Utils;
