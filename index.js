aa602bf78c98ce2319693c5ceed510a050e5cce5
Thomas Rasmussen <tsn1983@gmail.com>
2019-02-09 16:14:04 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-// module.exports = function removeSpace(string) {
-//     if (typeof string !== "string") throw new TypeError("Not a string!");
-//     return string.replace(/\s/g, "");
-//   };
+module.exports = function removeSpace(string) {
+    if (typeof string !== "string") throw new TypeError("Not a string!");
+    return string.replace(/\s/g, "");
+  };
 
-export class dtHelpers {
-  test() {
-    console.log('hello world');
-  }
-}
-
-export class regexHelpers{
-  regexTest(){
-    console.log("regex test");
-  }
-}
